9b0610681bc31e165605f255a642ad822c2de755
Igor Melo <imelo@essia.com>
2021-11-30 21:00:26 -0300

    allow user to interrupt with ctrl C

--- pullall.sh
+++ pullall.sh
@@ -1,6 +1,7 @@
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 CYAN='\033[0;36m'
+ORANGE='\033[0;33m'
 NC='\033[0m'
 
 max_parallel_jobs=10
@@ -16,7 +17,7 @@ function pull() {
   cd "$repo"
 
   out=$(git pull --all 2>&1)
-  changed=$(echo "$out" | grep -w "changed.")
+  msg=$(echo "$out" | egrep "changed\.|up to date\.")
 
   repos_left=$(cat "$working_dir/.repos_left")
   repos_left=$((repos_left - 1))
@@ -25,12 +26,23 @@ function pull() {
 
   if [ $? -eq 0 ]; then
     echo -e "$curr. ${GREEN}done${NC} -> $repo"
-    [ $changed ] && echo "$changed"
+    echo "$msg"
   else
     echo -e "$curr. ${RED}fail${NC} -> $repo"
     echo -e "$out"
   fi
+	echo
 }
+
+function onsigint() {
+  repos_left=$(cat "$working_dir/.repos_left")
+  echo
+	echo -e "${ORANGE}Script interrupted by user${NC}"
+	echo -e "${RED}$repos_left${NC} repositories weren't updated"
+  exit 2
+}
+
+trap onsigint 2
 
 folders=( $(ls -d $pattern | sed -r 's/\/\.git$//g') )
 
@@ -44,6 +56,7 @@ repos_left=${#folders[@]}
 echo "$repos_left" > "$working_dir/.repos_left"
 
 echo -e "Starting to pull from ${CYAN}$total_repos${NC} repositories"
+echo
 
 for folder in ${folders[@]}; do
   parallel_jobs=$(jobs | wc -l)
