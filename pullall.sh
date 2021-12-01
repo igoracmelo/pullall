caf853f966ca34b55f85392e743ad1d7354ecafd
Igor Melo <imelo@essia.com>
2021-12-01 13:01:34 -0300

    show repositories that weren't updated on sigint

--- pullall.sh
+++ pullall.sh
@@ -4,7 +4,7 @@ CYAN='\033[0;36m'
 ORANGE='\033[0;33m'
 NC='\033[0m'
 
-max_parallel_jobs=10
+max_parallel_jobs=15
 pattern="*"
 
 [ "$1" ] && pattern="$1"
@@ -12,33 +12,37 @@ pattern="*"
 
 pattern="$pattern/.git"
 
-function pull() {  
+function pull() {
   repo="$1"
   cd "$repo"
 
   out=$(git pull --all 2>&1)
-  msg=$(echo "$out" | egrep "changed\.|up to date\.")
+  code="$?"
+  msg=$(echo "$out" | egrep "changed|insertions|deletions|up to date\.")
 
-  repos_left=$(cat "$working_dir/.repos_left")
-  repos_left=$((repos_left - 1))
-  echo "$repos_left" > "$working_dir/.repos_left" 
-  curr=$((total_repos - repos_left))
+  repos_left=$(cat "$working_dir/.repos_left" | grep -vxF "$repo")
+  repos_count=$(echo -n "$repos_left" | grep -c ^)
 
-  if [ $? -eq 0 ]; then
+  echo -n "$repos_left" > "$working_dir/.repos_left"
+  curr=$((total_repos - repos_count))
+
+  if [ $code -eq 0 ]; then
     echo -e "$curr. ${GREEN}done${NC} -> $repo"
-    echo "$msg"
+    [ "$msg" ] && echo "$msg" || echo "$out"
   else
     echo -e "$curr. ${RED}fail${NC} -> $repo"
     echo -e "$out"
   fi
-	echo
+  echo
 }
 
 function onsigint() {
-  repos_left=$(cat "$working_dir/.repos_left")
+  repos=( $(cat "$working_dir/.repos_left") )
   echo
-	echo -e "${ORANGE}Script interrupted by user${NC}"
-	echo -e "${RED}$repos_left${NC} repositories weren't updated"
+  echo -e "${ORANGE}Script interrupted by user${NC}"
+  echo -e "${RED}${#repos[@]}${NC} repositories weren't updated:"
+  echo "${repos[@]}"
+  rm "$working_dir/.repos_left"
   exit 2
 }
 
@@ -52,14 +56,17 @@ export total_repos
 working_dir=$(pwd)
 export working_dir
 
-repos_left=${#folders[@]}
-echo "$repos_left" > "$working_dir/.repos_left"
+echo -n "" > .repos_left
 
-echo -e "Starting to pull from ${CYAN}$total_repos${NC} repositories"
+for folder in ${folders[@]}; do
+  echo $folder >> "$working_dir/.repos_left"
+done
+
+echo -e "Starting to pull from ${CYAN}${#folders[@]}${NC} repositories"
 echo
 
 for folder in ${folders[@]}; do
-  parallel_jobs=$(jobs | wc -l)
+  parallel_jobs=$(jobs | grep -c ^)
   while [ $parallel_jobs -gt $max_parallel_jobs ]; do
     sleep 0.3
   done
